d78a631f1f3040e56f47fbfa670c0de53323b455
tejasvi-shetter1 <tvshetter@gmail.com>
2024-04-28 08:21:27 +0530

    create a signup route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,8 +12,12 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 });
 
 app.use('/api/user',userRoutes);
+
+app.use('/api/auth',authRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,9 +16,10 @@ const userSchema = new mongoose.Schema({
         required:true
     }, 
 },
-    {timestamps:true});
+    {timestamps:true}
+);
 
 const User=mongoose.model('User',userSchema);
 
-export default User
+export default User;
    
